7b36e97dcc472d9669c084632c4d1cdb1b7ce9fb
mchao409 <mchao409@gmail.com>
2018-02-27 18:53:23 -0500

    change urls in 'urls.py' to regex

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -5,26 +5,26 @@ from authenticate import googledrive_views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-	path('', views.index,name='index'),
-	path('home/',views.index,name='index'),
-	path('signup/', views.signup, name='signup'),
-	path('logout/', views.log_out, name='logout'), # need to create
+	re_path('', views.index,name='index'),
+	re_path('home/?',views.index,name='index'),
+	re_path('signup/?', views.signup, name='signup'),
+	re_path('logout/?', views.log_out, name='logout'), # need to create
 	# path('login', auth_views.LoginView.as_view(), name='login')
-	path('login/', views.log_in,name='login'),
+	re_path('login/?', views.log_in,name='login'),
 
 	# Dropbox views
-	path('dropbox/redirect/', dropbox_views.redirect_away, name="dropbox_redirect"),
-	path('dropbox/save_token/', dropbox_views.save_token,name="dropbox_save_token"),
-	path('dropbox/list_folder/', dropbox_views.list_folder, name='dropbox_list_folder'),
-	path('dropbox/update_local/', dropbox_views.update_local, name='dropbox_update_local'),
+	re_path('dropbox/redirect/?', dropbox_views.redirect_away, name="dropbox_redirect"),
+	re_path('dropbox/save_token/?', dropbox_views.save_token,name="dropbox_save_token"),
+	re_path('dropbox/list_folder/?', dropbox_views.list_folder, name='dropbox_list_folder'),
+	re_path('dropbox/update_local/?', dropbox_views.update_local, name='dropbox_update_local'),
 	re_path('dropbox/update_remote/?', dropbox_views.update_remote, name='dropbox_update_remote'),
 
 	# Google Drive views
-	path('googledrive/redirect/', googledrive_views.redirect_away, name='googledrive_redirect'),
-	path('googledrive/code', googledrive_views.code, name='googledrive_code'),
-	path('googledrive/gettoken', googledrive_views.get_new_token, name='googledrive_gettoken'),
-	path('googledrive/list_folder', googledrive_views.list_folder, name='googledrive_list_folder'),
-	path('googledrive/update_local', googledrive_views.update_local, name='googledrive_list_folder'),
-	path('googledrive/update_remote', googledrive_views.update_remote, name='googledrive_update_remote'),
+	re_path('googledrive/redirect/?', googledrive_views.redirect_away, name='googledrive_redirect'),
+	re_path('googledrive/code/?', googledrive_views.code, name='googledrive_code'),
+	re_path('googledrive/gettoken/?', googledrive_views.get_new_token, name='googledrive_gettoken'),
+	re_path('googledrive/list_folder/?', googledrive_views.list_folder, name='googledrive_list_folder'),
+	re_path('googledrive/update_local/?', googledrive_views.update_local, name='googledrive_list_folder'),
+	re_path('googledrive/update_remote/?', googledrive_views.update_remote, name='googledrive_update_remote'),
 
 ]
